38f14b43a419e1686abd1d662759cbd5d4e61ff6
salsabeelomar <omarsalsabeel65@gmail.com>
2022-08-24 10:18:55 +0300

    create fetch function relates #5

---
+++ public/js/fetch.js
@@ -0,0 +1,7 @@
+const fetchData = (method, data, endpoint) => {
+  fetch(endpoint, {
+    method,
+    body: JSON.stringify(data),
+    headers: { 'Content-type': 'application/json' },
+  }).then((data) => res.json(data));
+};
